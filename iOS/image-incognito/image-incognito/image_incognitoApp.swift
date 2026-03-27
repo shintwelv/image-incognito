d3290e922b23be58bf2eaebd37146d8852603df7
shintwelv <shintwl@icloud.com>
2026-03-27 15:27:53 +0900

    project: created ios app project

---
+++ iOS/image-incognito/image-incognito/image_incognitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_incognitoApp.swift
+//  image-incognito
+//
+//  Created by elvin on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct image_incognitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/image-incognito/image-incognitoTests/image_incognitoTests.swift
@@ -0,0 +1,19 @@
+//
+//  image_incognitoTests.swift
+//  image-incognitoTests
+//
+//  Created by elvin on 3/27/26.
+//
+
+import Testing
+@testable import image_incognito
+
+struct image_incognitoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
