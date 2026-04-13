fc27031894862bdfaba503dfccb89cca483020ce
shintwelv <shintwl@icloud.com>
2026-04-13 09:25:56 +0900

    test: removed unused ui tests

--- iOS/image-incognito/image-incognitoUITests/image_incognitoUITests.swift
+++ iOS/image-incognito/image-incognitoUITests/image_incognitoUITests.swift
@@ -21,23 +21,4 @@ final class image_incognitoUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // XCUIAutomation Documentation
-        // https://developer.apple.com/documentation/xcuiautomation
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
-    }
 }
--- iOS/image-incognito/image-incognitoUITests/image_incognitoUITestsLaunchTests.swift
+++ iOS/image-incognito/image-incognitoUITests/image_incognitoUITestsLaunchTests.swift
@@ -16,20 +16,4 @@ final class image_incognitoUITestsLaunchTests: XCTestCase {
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
-
-    @MainActor
-    func testLaunch() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-        // XCUIAutomation Documentation
-        // https://developer.apple.com/documentation/xcuiautomation
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
-    }
 }
